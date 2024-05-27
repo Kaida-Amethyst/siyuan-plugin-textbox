b7d80c71ba3fd79f7458d9c9360bfdd1971cb3ab
Kaida Amethyst <ziyue.liu.vm@gmail.com>
2024-05-27 23:28:29 +0800

    try to add settings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Plugin, showMessage, fetchSyncPost } from "siyuan"
+import { Plugin, showMessage, fetchSyncPost, Dialog } from "siyuan"
 import "@/index.scss";
+import SettingExample from "@/setting-example.svelte";
 
 export default class PluginSample extends Plugin {
 
@@ -9,6 +10,12 @@ export default class PluginSample extends Plugin {
   async onload() {
     console.log("Load Plugin Text Box");
 
+    this.addIcons(`<symbol id="iconFace" viewBox="0 0 32 32">
+<path d="M13.667 17.333c0 0.92-0.747 1.667-1.667 1.667s-1.667-0.747-1.667-1.667 0.747-1.667 1.667-1.667 1.667 0.747 1.667 1.667zM20 15.667c-0.92 0-1.667 0.747-1.667 1.667s0.747 1.667 1.667 1.667 1.667-0.747 1.667-1.667-0.747-1.667-1.667-1.667zM29.333 16c0 7.36-5.973 13.333-13.333 13.333s-13.333-5.973-13.333-13.333 5.973-13.333 13.333-13.333 13.333 5.973 13.333 13.333zM14.213 5.493c1.867 3.093 5.253 5.173 9.12 5.173 0.613 0 1.213-0.067 1.787-0.16-1.867-3.093-5.253-5.173-9.12-5.173-0.613 0-1.213 0.067-1.787 0.16zM5.893 12.627c2.28-1.293 4.040-3.4 4.88-5.92-2.28 1.293-4.040 3.4-4.88 5.92zM26.667 16c0-1.040-0.16-2.040-0.44-2.987-0.933 0.2-1.893 0.32-2.893 0.32-4.173 0-7.893-1.92-10.347-4.92-1.4 3.413-4.187 6.093-7.653 7.4 0.013 0.053 0 0.12 0 0.187 0 5.88 4.787 10.667 10.667 10.667s10.667-4.787 10.667-10.667z"></path>
+</symbol>
+<symbol id="iconSaving" viewBox="0 0 32 32">
+<path d="M20 13.333c0-0.733 0.6-1.333 1.333-1.333s1.333 0.6 1.333 1.333c0 0.733-0.6 1.333-1.333 1.333s-1.333-0.6-1.333-1.333zM10.667 12h6.667v-2.667h-6.667v2.667zM29.333 10v9.293l-3.76 1.253-2.24 7.453h-7.333v-2.667h-2.667v2.667h-7.333c0 0-3.333-11.28-3.333-15.333s3.28-7.333 7.333-7.333h6.667c1.213-1.613 3.147-2.667 5.333-2.667 1.107 0 2 0.893 2 2 0 0.28-0.053 0.533-0.16 0.773-0.187 0.453-0.347 0.973-0.427 1.533l3.027 3.027h2.893zM26.667 12.667h-1.333l-4.667-4.667c0-0.867 0.12-1.72 0.347-2.547-1.293 0.333-2.347 1.293-2.787 2.547h-8.227c-2.573 0-4.667 2.093-4.667 4.667 0 2.507 1.627 8.867 2.68 12.667h2.653v-2.667h8v2.667h2.68l2.067-6.867 3.253-1.093v-4.707z"></path>
+</symbol>`);
     this.addIcons(`<svg id="iconTextArea" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-textarea-t" viewBox="0 0 16 16">
   <path d="M1.5 2.5A1.5 1.5 0 0 1 3 1h10a1.5 1.5 0 0 1 1.5 1.5v3.563a2 2 0 0 1 0 3.874V13.5A1.5 1.5 0 0 1 13 15H3a1.5 1.5 0 0 1-1.5-1.5V9.937a2 2 0 0 1 0-3.874zm1 3.563a2 2 0 0 1 0 3.874V13.5a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V9.937a2 2 0 0 1 0-3.874V2.5A.5.5 0 0 0 13 2H3a.5.5 0 0 0-.5.5zM2 7a1 1 0 1 0 0 2 1 1 0 0 0 0-2m12 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
   <path d="M11.434 4H4.566L4.5 5.994h.386c.21-1.252.612-1.446 2.173-1.495l.343-.011v6.343c0 .537-.116.665-1.049.748V12h3.294v-.421c-.938-.083-1.054-.21-1.054-.748V4.488l.348.01c1.56.05 1.963.244 2.173 1.496h.386z"/>
@@ -46,6 +53,22 @@ export default class PluginSample extends Plugin {
     let post_data = { id: blockId, attrs:attrs_data };
     let response = await fetchSyncPost(url, post_data);
     let res = response.code === 0 ? response.data : null;
+  };
+
+  private userSetting() : void {
+    let dialog = new Dialog({
+        title: "SettingPannel",
+        content: `<div id="SettingPanel" style="height: 100%;"></div>`,
+        width: "800px",
+        destroyCallback: (options) => {
+            console.log("destroyCallback", options);
+            //You'd better destroy the component when the dialog is closed
+            pannel.$destroy();
+        }
+    });
+    let pannel = new SettingExample({
+        target: dialog.element.querySelector("#SettingPanel"),
+    });
   }
 
   private blockIconEvent({ detail }: any) {
@@ -71,6 +94,13 @@ export default class PluginSample extends Plugin {
           this.setupTextBoxAttr(nodeId, "");
         }
       },
+      {
+        icon: "iconFace",
+        label: "Settings",
+        click: () => {
+          this.userSetting();
+        }
+      }
     ];
     detail.menu.addItem ({
       icon: "iconTextArea",
